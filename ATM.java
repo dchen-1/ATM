a430a6e3175089ff58b79a5936335c3db394ea3f
David <dchen1561@gmail.com>
2023-09-01 23:21:06 -0700

    Minor changes

--- ATM.java
+++ ATM.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.InaccessibleObjectException;
+import java.nio.file.*;
 import java.sql.SQLDataException;
 import java.util.HashMap;
 
@@ -16,39 +18,41 @@ public class ATM{
         map.put(userID,amount);
     }
     }
-    public void closeAccount(String userID) throws IllegalAccessException{
-            if(map.get(userID)>0){
-                throw new IllegalAccessException("You still have money in the account");
+    public void closeAccount(String userID) throws ArithmeticException{
+            if(!map.containsKey(userID)||map.get(userID)>0){
+                throw new ArithmeticException("You still have money in the account");
             }
             else{
                 map.remove(userID);
             }
     }
     public double checkBalance(String userID) throws NullPointerException{
-        if(map.get(userID)==0) throw new NullPointerException("What money?");
+        if(map.get(userID)==0) throw new NullPointerException("You are Broke");
         return map.get(userID);
     }
-    public double depositMoney(String userID,double amount) throws NullPointerException{
-        if(!map.containsKey(userID)) throw new NullPointerException("What money?");
+    public double depositMoney(String userID,double amount) {
+        if(!map.containsKey(userID)) throw new InaccessibleObjectException("Error 404");
         map.put(userID,map.get(userID)+amount);
         return amount;
     }
 
-    public double withdrawMoney(String userID, double amount) throws NullPointerException{
-        if(map.get(userID)<amount){
-            throw new NullPointerException("You can't withdraw money you don't have");
+    public double withdrawMoney(String userID, double amount){
+        if(!map.containsKey(userID)||map.get(userID)<amount){
+            throw new InaccessibleObjectException("You can't withdraw money you don't have");
         }
         map.put(userID,map.get(userID)-amount);
-        return amount;
+        return map.get(userID);
     }
 
-    public boolean transferMoney(String fromAccount, String toAccount, double amount){
+    public boolean transferMoney(String fromAccount, String toAccount, double amount) throws IllegalAccessException{
         withdrawMoney(fromAccount, amount);
         depositMoney(toAccount, amount);
         return true;
     }
 
-    public void audit(String fileName) throws IOException{
+    public void audit() throws IOException{
+        final String fileName = "AccountAudit.txt";
+        Files.deleteIfExists(Paths.get(fileName));
         PrintWriter pw = new PrintWriter(fileName);
         for(String str : map.keySet()){
             pw.write(str+":"+map.get(str)+"\n");
